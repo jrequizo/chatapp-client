248cd8a0b9fc739e20d5141bf6bab06efeaa4d02
jrequizo <jay.requizo@outlook.com>
2022-08-06 16:27:14 +1000

    Use right version of trpc

--- src/utils/trpc/trpc.ts
+++ src/utils/trpc/trpc.ts
@@ -4,7 +4,7 @@ import { createReactQueryHooks } from "@trpc/react";
  * If you're pushing to staging, then uncomment the first import which imports from GitHub.
  * If you're testing locally, use the second import to view live changes from your /packages/api directory.
  */
-// import { AppRouter } from "chatapp-api";
-import { AppRouter } from "chatapp-api-dev";
+import { AppRouter } from "chatapp-api";
+// import { AppRouter } from "chatapp-api-dev";
 
 export const API = createReactQueryHooks<AppRouter>();
